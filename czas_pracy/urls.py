eedcb3e167f7c0a41e76e2fb9092aba672c8cfed
Damian Suwała <damian.suwala@gmail.com>
2020-05-01 18:34:18 +0200

    simply register page

--- czas_pracy/urls.py
+++ czas_pracy/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from time_registration.views import index
+from time_registration.views import index, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register),
     path('', index)
 ]
--- time_registration/views.py
+++ time_registration/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+from .forms import CreateUserForm
 
 
 def index(request):
     context = {}
     return render(request, 'panel.html', context)
+
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Pomyślnie założono konto: ' + username)
+            form.clean()
+    context = {'form': form}
+    return render(request, 'register.html', context)
