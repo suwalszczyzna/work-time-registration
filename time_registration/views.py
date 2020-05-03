94d400e898e0031fdc17404fe8d333ff198d63fa
Damian Suwała <damian.suwala@gmail.com>
2020-05-03 19:58:33 +0200

    remove unnecessary print from login_page view

--- time_registration/views.py
+++ time_registration/views.py
@@ -81,7 +81,6 @@ def login_page(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        print(user)
         if user is not None:
             login(request, user)
             return redirect('home')
