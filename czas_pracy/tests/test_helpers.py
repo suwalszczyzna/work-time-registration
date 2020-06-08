9952ea69cbc92a75a47f3b1f57d2b9d0adb6c4ad
Damian Suwała <damian.suwala@gmail.com>
2020-06-08 22:58:43 +0200

    refactor test_helpers.py

--- czas_pracy/tests/test_helpers.py
+++ czas_pracy/tests/test_helpers.py
@@ -1,35 +1,38 @@
 from mixer.backend.django import mixer
 from time_registration.helpers import get_employee_by_user_id, is_register_owner, is_employed, plan_leaving_hours
 from time_registration.models import TimeRegistration
+from django.test import TestCase
 import pytest
 import datetime
 
 
 @pytest.mark.django_db
-class TestHelpers:
+class TestHelpers(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestHelpers, cls).setUpClass()
+        cls.time_registration: TimeRegistration = mixer.blend('time_registration.TimeRegistration')
+        cls.time_registration2: TimeRegistration = mixer.blend('time_registration.TimeRegistration')
+        cls.employee = mixer.blend('time_registration.Employee')
+        cls.employee2 = mixer.blend('time_registration.Employee')
+
     def test_is_register_owner(self):
-        time_registration: TimeRegistration = mixer.blend('time_registration.TimeRegistration')
-        time_registration2: TimeRegistration = mixer.blend('time_registration.TimeRegistration')
-        user_id: int = time_registration.employee.user.id
-        assert is_register_owner(user_id, time_registration)
-        assert not is_register_owner(user_id, time_registration2)
+        user_id: int = self.time_registration.employee.user.id
+        assert is_register_owner(user_id, self.time_registration)
+        assert not is_register_owner(user_id, self.time_registration2)
 
     def test_get_employee_by_user_id(self):
-        employee = mixer.blend('time_registration.Employee')
-        assert get_employee_by_user_id(employee.user.id) == employee
+        assert get_employee_by_user_id(self.employee.user.id) == self.employee
 
     def test_dont_get_employee_by_user_id(self):
-        employee = mixer.blend('time_registration.Employee')
-        assert get_employee_by_user_id(0) != employee
+        assert get_employee_by_user_id(0) != self.employee
 
     def test_false_get_employee_by_user_id(self):
-        employee = mixer.blend('time_registration.Employee')
-        employee2 = mixer.blend('time_registration.Employee')
-        assert get_employee_by_user_id(employee.user.id) != employee2
+        assert get_employee_by_user_id(self.employee.user.id) != self.employee2
 
     def test_is_employed(self):
-        employee = mixer.blend('time_registration.Employee')
-        assert is_employed(employee.user.id)
+        assert is_employed(self.employee.user.id)
 
     def test_is_not_employed(self):
         assert not is_employed(0)
