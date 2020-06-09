26a05058bd22b1e18ef67d98e9b7ed739020ab44
Damian Suwała <damian.suwala@gmail.com>
2020-06-09 17:48:54 +0200

    change folder name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'czas_pracy.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'work_time.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- work_time/wsgi.py
+++ work_time/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for czas_pracy project.
+WSGI config for work_time project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'czas_pracy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'work_time.settings')
 
 application = get_wsgi_application()
