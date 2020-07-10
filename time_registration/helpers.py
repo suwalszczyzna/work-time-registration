94ac1a8171dae9d480575b922ad06f2f0c1979c2
Damian Suwała <damian.suwala@gmail.com>
2020-07-10 12:30:48 +0200

    deleted hours from modal, added helper method

--- time_registration/helpers.py
+++ time_registration/helpers.py
@@ -44,3 +44,28 @@ def plan_leaving_hour_with_brakes(time_registration: TimeRegistration) -> time:
     result_plan_leaving: datetime = _dumb_plan_leaving + timedelta(minutes=time_registration.brakes)
 
     return result_plan_leaving.time()
+
+
+def round_time(dt: datetime, date_delta: timedelta, to='average') -> datetime:
+    """
+    Round a datetime object to a multiple of a timedelta
+    dt : datetime.datetime object, default now.
+    dateDelta : timedelta object, we round to a multiple of this, default 1 minute.
+    from:  http://stackoverflow.com/questions/3463930/how-to-round-the-minute-of-a-datetime-object-python
+    """
+    round_to = date_delta.total_seconds()
+    seconds = (dt - dt.min).seconds
+
+    if seconds % round_to == 0 and dt.microsecond == 0:
+        rounding = (seconds + round_to / 2) // round_to * round_to
+    else:
+        if to == 'up':
+            # // is a floor division, not a comment on following line (like in javascript):
+            rounding = (seconds + dt.microsecond/1000000 +
+                        round_to) // round_to * round_to
+        elif to == 'down':
+            rounding = seconds // round_to * round_to
+        else:
+            rounding = (seconds + round_to / 2) // round_to * round_to
+
+    return dt + timedelta(0, rounding - seconds, - dt.microsecond)
