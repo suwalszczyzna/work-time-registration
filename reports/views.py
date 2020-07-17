9a3b8652c47ea4c31e2018cd7f0bbeaaccbd13e1
Damian Suwała <damian.suwala@gmail.com>
2020-07-17 11:04:00 +0200

    init monthly_report.html and monthly_report_view

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from datetime import datetime
+from free_days_registration.models import FreeDayRegistration
+from time_registration.models import TimeRegistration
 
-# Create your views here.
+
+def monthly_report_view(request) -> render:
+    date: datetime = datetime.now()
+    user_id = request.user.id
+
+    time_registrations = TimeRegistration.objects.filter(
+        date__month=date.month
+    )
+
+    free_day_registrations = FreeDayRegistration.objects.filter(
+        employee__user_id__exact=user_id,
+        status=FreeDayRegistration.Status.ACCEPTED,
+    )
+
+    context = {
+        'time_registrations': time_registrations,
+        'free_day_registrations': free_day_registrations
+    }
+    return render(request, 'reports/monthly_report.html', context)
