75054820c0ca6418b34d3faf13e4df4e6da15e00
Damian Suwała <damian.suwala@gmail.com>
2020-07-20 09:42:23 +0200

    improvement MonthlyReport classes and template

--- reports/reports.py
+++ reports/reports.py
@@ -5,17 +5,12 @@ from math import fabs
 
 from django.db.models import QuerySet
 
+from free_days_registration.models import FreeDayRegistration
 from time_registration.helpers import get_time_registration_by
 from time_registration.models import Employee, TimeRegistration
 
 
-class Report(ABC):
-    @abstractmethod
-    def rows(self) -> list:
-        pass
-
-
-class MonthlyReport(Report):
+class MonthlyReport:
 
     def __init__(
             self, 
@@ -23,39 +18,50 @@ class MonthlyReport(Report):
             emp: Employee,
             report_date: datetime,
     ) -> None:
+        self.free_day_registrations = free_day_registrations
+        self.emp = emp
         self.report_rows: list = []
         self.report_date: datetime = report_date
-        self.report_row_generator: MonthlyReportRow = MonthlyReportRow(free_day_registrations, emp)
+        self.generate_rows()
 
-    def rows(self) -> list:
+    def generate_rows(self):
 
         max_day = monthrange(self.report_date.year, self.report_date.month)[1]
 
         for day in range(1, max_day + 1):
             _date_to_row: datetime = datetime(self.report_date.year, self.report_date.month, day)
 
-            _time_registrations = get_time_registration_by(_date_to_row, self.report_row_generator.employee)
+            _time_registrations = get_time_registration_by(_date_to_row, self.emp)
             _tr: TimeRegistration = _time_registrations.filter(date__exact=_date_to_row).first()
 
-            self.report_row_generator.update_row(_date_to_row, _tr)
-            self.report_rows.append(self.report_row_generator.generate_row())
-        return self.report_rows
+            monthly_row = MonthlyReportRow(self.free_day_registrations, self.emp, _date_to_row, _tr)
+            self.report_rows.append(monthly_row)
 
 
 class MonthlyReportRow:
-    def __init__(self, _free_day_reg_set: QuerySet, _emp: Employee):
-        self.date = None
-        self.time_reg = None
-        self.free_day_reg_set = _free_day_reg_set
-        self.employee = _emp
-        self.realization_hour_sec = 0
-
-    def update_row(self, _date: datetime, _time_reg: TimeRegistration):
+    def __init__(self, _free_day_reg_set: QuerySet, _emp: Employee, _date: datetime, _time_reg: TimeRegistration):
         self.date = _date
         self.time_reg = _time_reg
+        self.free_day_reg_set = _free_day_reg_set
+        self.employee = _emp
+        self.realization_hour_sec = None
+        self.realization_time = None
+        self.overtime = None
+        self.lack = None
+        self.free_day_type = None
+        self.worked_day = None
 
-    @property
-    def realization(self) -> time:
+        self.update_values()
+
+    def update_values(self, ):
+        self.reset_values()
+        self.overtime = self.calc_overtime()
+        self.realization_time = self.calc_realization_time()
+        self.lack = self.calc_lack()
+        self.free_day_type = self.calc_free_day_type()
+        self.worked_day = self.is_worked_day()
+
+    def calc_realization_time(self) -> time:
         if isinstance(self.time_reg, TimeRegistration) and self.time_reg.arrival and self.time_reg.leaving:
             duration = datetime(
                 1, 1, 1, self.time_reg.leaving.hour, self.time_reg.leaving.minute
@@ -69,27 +75,22 @@ class MonthlyReportRow:
             return time(int(hours[0]), int(minutes[0]))
         return time()
 
-    @property
-    def overtime(self) -> timedelta:
+    def calc_overtime(self) -> timedelta:
         dif_realize_to_work_sec = self._realize_to_work_sec()
         if dif_realize_to_work_sec > 0:
             return timedelta(seconds=dif_realize_to_work_sec)
         return timedelta(seconds=0)
 
-    @property
-    def lack(self) -> timedelta:
+    def calc_lack(self) -> timedelta:
         dif_realize_to_work_sec = self._realize_to_work_sec()
         if dif_realize_to_work_sec < 0:
             return timedelta(seconds=fabs(dif_realize_to_work_sec))
         return timedelta(seconds=0)
 
-    @property
-    def free_day_type(self):
-        _free_day_type = '-'
+    def calc_free_day_type(self) -> FreeDayRegistration:
         for fdr in self.free_day_reg_set:
             if fdr.start_date <= self.date.date() <= fdr.end_date:
-                _free_day_type = fdr.free_day_type
-        return _free_day_type
+                return fdr.free_day_type
 
     def is_worked_day(self):
         _time_arrival = getattr(self.time_reg, 'arrival', None)
@@ -99,39 +100,11 @@ class MonthlyReportRow:
         else:
             return False
 
-    def generate_row(self) -> list:
-        self.reset_values()
-        row_date = self.date.strftime("%d.%m.%Y")
-        time_arrival = getattr(self.time_reg, 'arrival', '-')
-        time_leaving = getattr(self.time_reg, 'leaving', '-') or '-'
-        plan_leaving = getattr(self.time_reg, 'plan_leaving', '-')
-        brakes = getattr(self.time_reg, 'brakes', '0')
-        if self.is_worked_day():
-            overtime = self.overtime
-            lack = self.lack
-        else:
-            overtime = '-'
-            lack = '-'
-
-        row = [
-            row_date,
-            time_arrival,
-            time_leaving,
-            plan_leaving,
-            f'{brakes} min.',
-            self.realization,
-            overtime,
-            lack,
-            self.free_day_type
-        ]
-
-        return row
-
     def reset_values(self):
         self.realization_hour_sec = 0
 
     def _realize_to_work_sec(self):
-        _ = self.realization
+        _ = self.realization_time
         working_hours: int = self.employee.working_hours
         working_seconds = working_hours * 3600
         realize_to_work_sec = self.realization_hour_sec - working_seconds
--- reports/views.py
+++ reports/views.py
@@ -6,7 +6,7 @@ from free_days_registration.helpers import get_free_day_registrations_by
 from time_registration.decorators import employee_login_required
 from time_registration.helpers import get_employee_by_user_id
 from time_registration.models import Employee
-from .reports import MonthlyReport, Report
+from .reports import MonthlyReport
 
 
 @employee_login_required
@@ -15,11 +15,10 @@ def monthly_report_view(request):
     employee: Employee = get_employee_by_user_id(request.user.id)
     free_day_registrations = get_free_day_registrations_by(employee)
     report_date = datetime.now()
-    month_report: Report = MonthlyReport(free_day_registrations, employee, report_date)
-    month_report_rows = month_report.rows()
+    month_report: MonthlyReport = MonthlyReport(free_day_registrations, employee, report_date)
 
     context = {
         'report_date': report_date,
-        'rows': month_report_rows
+        'report_rows': month_report.report_rows
     }
     return render(request, 'reports/monthly_report.html', context)
